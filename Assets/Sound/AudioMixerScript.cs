45ba280c66c72d9c6864538e2307dd3ac68f7ccf
Dylano Burgers <dylano.burgers@outlook.com>
2023-09-18 10:31:02 +0200

    Music slider werkt!

--- Assets/Sound/AudioMixerScript.cs
+++ Assets/Sound/AudioMixerScript.cs
@@ -1,22 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UIElements;
 
 public class AudioMixerScript : MonoBehaviour
 {
-    public GameObject[] audioSliders;
-
-    public float sound;
+    public Slider audioSlider;
+    public GameObject slider;
+    public AudioMixer mixer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        MixerVolume();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void MixerVolume()
+    {
+        mixer.SetFloat("UiVolume", 1);
+        mixer.SetFloat("MusicVolume", 1);
+        mixer.SetFloat("SFXVolume", 1);
+        mixer.SetFloat("MasterVolume", 1);
+    }
+
+    public void MusicVolumeValue(float volume)
+    {
+        mixer.SetFloat("MusicVolume", volume);
+    }
+
+
 }
