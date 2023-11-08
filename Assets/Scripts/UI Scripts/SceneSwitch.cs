00264a128f4827f72989ace34c3c80716f93f530
Dylano Burgers <dylano.burgers@outlook.com>
2023-11-08 16:58:37 +0100

    oading screen ghemaakt en toegefoegt plus een aantal kleine UI elementen toegevoegt

--- Assets/Scripts/UI Scripts/SceneSwitch.cs
+++ Assets/Scripts/UI Scripts/SceneSwitch.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneSwitch : MonoBehaviour
 {
     public float seconds = 10f;
+    public Slider progressSlider;
+    public GameObject loadingScreen;
+    public GameObject menu;
 
     public void Start()
     {
@@ -13,10 +17,17 @@ public class SceneSwitch : MonoBehaviour
         {
             StartCoroutine(SwitchToMain(1));
         }
+
+        loadingScreen.SetActive(false);
     }
     public void ToSwitch(int sceneToLoad)
     {
         SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void Loadscreen(int sceneToLoad)
+    {
+        StartCoroutine(LoadSceneAsync(sceneToLoad));
     }
 
     public IEnumerator SwitchToMain(int sceneToLoad)
@@ -24,5 +35,28 @@ public class SceneSwitch : MonoBehaviour
         yield return new WaitForSeconds(seconds);
 
         SceneManager.LoadScene(sceneToLoad);
+    }
+
+    IEnumerator LoadSceneAsync(int levelInt)
+    {
+        menu.SetActive(false);
+        loadingScreen.SetActive(true);
+
+        AsyncOperation op = SceneManager.LoadSceneAsync(levelInt);
+
+        op.allowSceneActivation = false;
+
+        yield return new WaitForSeconds(5);
+
+        op.allowSceneActivation = true;
+
+        while (!op.isDone)
+        {
+            int numOfRoundedDecimals = 1;
+            float progress = Mathf.Clamp01(op.progress / .9f);
+            progress = Mathf.Round(progress * Mathf.Pow(10, numOfRoundedDecimals)) / Mathf.Pow(10, numOfRoundedDecimals);
+            progressSlider.value = progress;
+            yield return null;
+        }
     }
 }
