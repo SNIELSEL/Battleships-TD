f4d9e81259e4a1384276e496130d53aff6f8a9d8
Dylano Burgers <dylano.burgers@outlook.com>
2023-11-09 14:40:23 +0100

    kamikaze plane flight script werkent
    ik begin stemmen te horen in mijn hoofd

--- Assets/Scripts/Programming/PlaneScripts/SendPlanes/Pilot Your own Plane/KamikazeBomberPiloting.cs
+++ Assets/Scripts/Programming/PlaneScripts/SendPlanes/Pilot Your own Plane/KamikazeBomberPiloting.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
-
 public class KamikazeBomberPiloting : MonoBehaviour
 {
-    public float flightSpeed, speedCap, startSpeed, flightRotation, cameraSmooth, sens, camSwayHor, camSwayVer;
+    public float flightSpeed, speedCap, startSpeed, flightRotation, cameraSmooth, sens, camSwayHor, camSwayVer, angleCount;
     public Vector3 movement, rotation;
     public Transform tPPCamera;
     private quaternion target;
     private Rigidbody rb;
 
-    public Vector2 rotateAngle;
+    public Vector3 rotateAngle;
+
+    Vector3 angleV3;
 
     void Start()
     {
@@ -38,11 +39,25 @@ public class KamikazeBomberPiloting : MonoBehaviour
 
     public void Movement()
     {
-        movement.z = Input.GetAxis("Horizontal");
-
         movement.z = flightSpeed;
-
         transform.Translate(movement * Time.deltaTime);
+
+        float angle = Input.GetAxis("Horizontal");
+        angleV3.z = angle * -angleCount;
+        angleV3.y = angle * -angleCount;
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            angleV3.x = -angleCount;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            angleV3.x = angleCount;
+        }
+
+        transform.Rotate(angleV3 * Time.deltaTime);
+        angleV3.x = 0;
     }
 
     public void Rotation()
@@ -52,17 +67,7 @@ public class KamikazeBomberPiloting : MonoBehaviour
 
         transform.Rotate(rotation * Time.deltaTime * sens);
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(0, -rotateAngle.x, 0);
-            transform.Rotate(0, 0, -rotateAngle.y);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(0, rotateAngle.x, 0);
-            transform.Rotate(0, 0, rotateAngle.y);
-        }
+        rotateAngle.z = rotation.z * Time.deltaTime;
 
         Quaternion rotationX = Quaternion.AngleAxis(-rotation.z * camSwayHor, Vector3.right * Time.deltaTime);
         Quaternion rotationY = Quaternion.AngleAxis(rotation.x * camSwayVer, Vector3.up * Time.deltaTime);
